ce96260e65fed344a8623fca5b69f91841b7f4c2
noel-lopez <nlopez.publi@gmail.com>
2023-08-09 13:48:16 +0200

    Renamed tagTypes

--- src/components/Tag/tagTypes.ts
+++ src/components/Tag/tagTypes.ts
@@ -3,12 +3,13 @@ interface TagClassMap {
 }
 
 export const tagClassMap: TagClassMap = {
+  html: "bg-orange-100 text-orange-700",
   css: "bg-blue-100 text-blue-700",
   javascript: "bg-yellow-200 text-yellow-800",
-  html: "bg-orange-100 text-orange-700",
   react: "bg-cyan-100 text-cyan-600",
   design: "bg-indigo-100 text-indigo-700",
   web: "bg-slate-200 text-slate-700",
+  productividad: "bg-red-100 text-red-700",
   node: "bg-lime-100 text-lime-700",
   next: "bg-violet-100 text-violet-700",
   git: "bg-amber-100 text-amber-700",
