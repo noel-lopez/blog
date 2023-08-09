2d76ec9501923ad4aeee28738a783be42d0f94f3
noel-lopez <nlopez.publi@gmail.com>
2023-08-09 17:31:56 +0200

    Added code hightlight components

--- src/app/blog/[slug]/postComponents.tsx
+++ src/app/blog/[slug]/postComponents.tsx
@@ -1,5 +1,9 @@
 import Image, { ImageProps } from "next/image";
 import Link, { LinkProps } from "next/link";
+import { Code } from "bright";
+import codeTheme from "./codeTheme.json";
+
+Code.theme = codeTheme;
 
 export const components = {
   h1: (props: React.HTMLProps<HTMLHeadingElement>) => (
@@ -9,10 +13,10 @@ export const components = {
     />
   ),
   h2: (props: React.HTMLProps<HTMLHeadingElement>) => (
-    <h2 {...props} className="font-medium mt-8 sm:text-3xl text-xl" />
+    <h2 {...props} className="font-medium mt-5 sm:text-3xl text-xl" />
   ),
   h3: (props: React.HTMLProps<HTMLHeadingElement>) => (
-    <h3 {...props} className="font-medium mt-6 sm:text-2xl text-lg" />
+    <h3 {...props} className="font-medium mt-4 sm:text-2xl text-lg" />
   ),
   Image: (props: ImageProps) => (
     <Image
@@ -24,10 +28,23 @@ export const components = {
   Link: (props: LinkProps) => (
     <Link
       {...props}
-      className="hover:underline hover:text-blue-600 text-blue-600 hover:bg-sky-50/50"
+      className="hover:underline hover:text-blue-500 text-blue-400"
     />
   ),
   blockquote: (props: React.HTMLProps<HTMLQuoteElement>) => (
     <blockquote {...props} className="bg-blue-50 p-4" />
+  ),
+  pre: (props: React.HTMLProps<HTMLPreElement>) => (
+    <Code {...props} />
+  ),
+  // Inline code
+  Inline: (props: React.HTMLProps<HTMLElement>) => (
+    <code {...props} className="bg-blue-50 p-0.5 border border-blue-300 text-black/95 rounded-lg inline-block " />
+  ),
+  ul: (props: React.HTMLProps<HTMLUListElement>) => (
+    <ul {...props} className="list-disc list-inside" />
+  ),
+  li: (props: React.HTMLProps<HTMLLIElement>) => (
+    <li {...props} className="mb-2 list-item" />
   ),
 };
