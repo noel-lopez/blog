d477aa8dbfd66a00eeb27ed8ffc29743b07a88b3
noel-lopez <nlopez.publi@gmail.com>
2023-08-09 12:36:04 +0200

    Added more tags

--- src/components/Tag/controller.ts
+++ src/components/Tag/controller.ts
@@ -2,7 +2,7 @@ interface TagClassMap {
   [key: string]: string;
 }
 
-const tagClassMap: TagClassMap = {
+export const tagClassMap: TagClassMap = {
   css: "bg-blue-100 text-blue-700",
   javascript: "bg-yellow-200 text-yellow-800",
   html: "bg-orange-100 text-orange-700",
@@ -10,7 +10,10 @@ const tagClassMap: TagClassMap = {
   design: "bg-indigo-100 text-indigo-700",
   web: "bg-slate-200 text-slate-700",
   node: "bg-lime-100 text-lime-700",
+  next: "bg-violet-100 text-violet-700",
   git: "bg-amber-100 text-amber-700",
+  seo: "bg-rose-100 text-rose-700",
+  npm: "bg-emerald-100 text-emerald-700",
   personal: "bg-blue-100 text-blue-700",
 };
 
