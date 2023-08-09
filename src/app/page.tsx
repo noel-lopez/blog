d3e6f07b4cdce5432690a6ebdf5ea0fc0cd68008
noel-lopez <nlopez.publi@gmail.com>
2023-08-09 12:37:23 +0200

    Moved header to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/Header";
 import PostCard from "@/components/PostCard";
+import TagsFilter from "@/components/TagsFilter";
 import { getAllFilesMetadata } from "@/lib/mdx";
 import { Post } from "@/shared/types";
 
@@ -9,28 +9,26 @@ interface HomeProps {
 
 function Home({ posts }: HomeProps) {
   return (
-    <div>
-      <Header />
-      <main className="max-w-5xl mx-auto p-4 sm:p-8 mb-16 grid gap-16">
-        <header className="flex flex-col items-center gap-2 py-8">
-          <h1 className="font-bold text-4xl sm:text-6xl text-center tracking-tighter leading-tight selection:bg-black selection:text-white">
-            Desarrollo y Diseño Web
-          </h1>
-          <h2 className="text-center selection:bg-black selection:text-white">
-            Soy{" "}
-            <span className="text-blue-300 selection:bg-black selection:text-blue-100">
-              Noel Lopez
-            </span>{" "}
-            y hablo de tecnología, programación y experiencias personales
-          </h2>
-        </header>
-        <section className="grid grid-cols-[repeat(auto-fill,minmax(min(350px,100%),1fr))] gap-4 sm:gap-8">
-          {posts.map((post) => (
-            <PostCard key={post.slug} data={post} />
-          ))}
-        </section>
-      </main>
-    </div>
+    <main className="max-w-5xl mx-auto p-4 sm:p-8 mb-16 grid gap-16">
+      <header className="flex flex-col items-center gap-2 py-8">
+        <h1 className="font-bold text-4xl sm:text-6xl text-center tracking-tighter leading-tight selection:bg-black selection:text-white">
+          Desarrollo y Diseño Web
+        </h1>
+        <h2 className="text-center selection:bg-black selection:text-white">
+          Soy{" "}
+          <span className="text-blue-300 selection:bg-black selection:text-blue-100">
+            Noel Lopez
+          </span>{" "}
+          y hablo de tecnología, programación y experiencias personales
+        </h2>
+      </header>
+      <TagsFilter />
+      <section className="grid grid-cols-[repeat(auto-fill,minmax(min(350px,100%),1fr))] gap-4 sm:gap-8">
+        {posts.map((post) => (
+          <PostCard key={post.slug} data={post} />
+        ))}
+      </section>
+    </main>
   );
 }
 
