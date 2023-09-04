4d2ea48d3dac5a71338b0e57ac5eb233801be48d
Noel Lopez <nlopez.publi@gmail.com>
2023-09-04 10:13:11 +0200

    feat: add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="border-t">
+      <p className="max-w-5xl mx-auto px-4 text-center py-12 font-semibold">
+        Made with 💜 by Noel Lopez
+      </p>
+    </footer>
+  )
+}
